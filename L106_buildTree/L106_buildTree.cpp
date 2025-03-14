b35b539d55f02d723fff2b048597d030bb70b0bc
garen_lee <srljl@163.com>
2025-03-14 13:43:01 +0800

    code form

--- L106_buildTree/L106_buildTree.cpp
+++ L106_buildTree/L106_buildTree.cpp
@@ -13,7 +13,7 @@
 
 #include "L106_buildTree.h"
 
-TreeNode* L106_buildTree::helper(int in_left, int in_right, vector<int>& inorder, vector<int>& postorder){
+TreeNode *L106_buildTree::helper(int in_left, int in_right, vector<int> &inorder, vector<int> &postorder) {
     // 如果这里没有节点构造二叉树了，就结束
     if (in_left > in_right) {
         return nullptr;
@@ -21,7 +21,7 @@ TreeNode* L106_buildTree::helper(int in_left, int in_right, vector<int>& inorder
 
     // 选择 post_idx 位置的元素作为当前子树根节点
     int root_val = postorder[post_idx];
-    TreeNode* root = new TreeNode(root_val);
+    TreeNode *root = new TreeNode(root_val);
 
     // 根据 root 所在位置分成左右两棵子树
     int index = idx_map[root_val];
@@ -35,20 +35,21 @@ TreeNode* L106_buildTree::helper(int in_left, int in_right, vector<int>& inorder
     return root;
 }
 
-TreeNode *L106_buildTree::buildTree(vector<int>& inorder, vector<int>& postorder) {
+TreeNode *L106_buildTree::buildTree(vector<int> &inorder, vector<int> &postorder) {
 // 从后序遍历的最后一个元素开始
-    post_idx = (int)postorder.size() - 1;
+    post_idx = (int) postorder.size() - 1;
 
     // 建立（元素，下标）键值对的哈希表
     int idx = 0;
-    for (auto& val : inorder) {
+    for (auto &val : inorder) {
         idx_map[val] = idx++;
     }
-    return helper(0, (int)inorder.size() - 1, inorder, postorder);
+    return helper(0, (int) inorder.size() - 1, inorder, postorder);
 }
+
 void L106_buildTree::test() {
-    vector<int> inorder = {9,3,15,20,7};
-    vector<int> postorder = {9,15,7,20,3};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    vector<int> postorder = {9, 15, 7, 20, 3};
     TreeNode *root = buildTree(inorder, postorder);
     cout << print_tree(root) << endl;
     inorder = {-1};
